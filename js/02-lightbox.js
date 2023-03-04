9ebe14b7649b49ac7ca44328a6e640c324568e20
Heorhii <gerythepatient@gmail.com>
2023-03-04 18:54:27 +0100

    Add task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,8 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+new SimpleLightbox('.gallery a', { 
+    captionsData: 'alt',
+    captionDelay: 250,
+ });
+
