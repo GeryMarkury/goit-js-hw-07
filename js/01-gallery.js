43fddb940109f5303f124f5add76227e5816764a
Heorhii <gerythepatient@gmail.com>
2023-03-04 17:51:53 +0100

    Add first task

---
+++ js/01-gallery.js
@@ -0,0 +1,35 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const galleryEl = document.querySelector('.gallery');
+
+
+galleryEl.addEventListener('click', event => {
+    event.preventDefault();
+
+    if (event.target.nodeName !== 'IMG') {
+    return;
+    };
+
+    const imageLink = event.target.dataset.source;
+    
+const modalEl = basicLightbox.create(`
+    <div class="modal">
+        <img src='${imageLink}' width="800" height="600">
+    </div>
+`);
+
+    modalEl.show();
+
+galleryEl.addEventListener("keydown", event => {
+    const isVisible = modalEl.visible();
+
+    if (isVisible && event.key === 'Escape') {
+        modalEl.close();
+    };
+});
+});
+
+
+
+
